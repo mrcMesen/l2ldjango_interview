3eb800bc1626ac22479f990db8ac9b8dc9787ca9
Marco Mesen <marcokse@hotmail.es>
2023-11-01 15:14:52 -0600

    feat: using filter in template

--- l2l/views.py
+++ l2l/views.py
@@ -3,12 +3,11 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.template import loader
 
+from common.utils.dates import IS0_FORMAT
+
 
 def index(request):
     now = datetime.now()
-    template = loader.get_template('l2l/index.html')
-    context = {
-        'iso': now.strftime("%Y-%m-%dT%H:%M:%S"),
-        'now': now
-    }
+    template = loader.get_template("l2l/index.html")
+    context = {"iso": now.strftime(IS0_FORMAT), "now": now}
     return HttpResponse(template.render(context, request))
